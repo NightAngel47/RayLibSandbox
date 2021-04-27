9c502d23a263bfdbd0041cba012d68b719e754ab
Steven Drovie <sjdrovie@gmail.com>
2021-04-26 22:05:32 -0500

    Basic Window with raylib and raylib-cpp

--- RayLibSandbox/RayLibSandbox/src/RayLibSandbox.cpp
+++ RayLibSandbox/RayLibSandbox/src/RayLibSandbox.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include "raylib-cpp.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int screenWidth = 800;
+    int screenHeight = 450;
 
-	std::cin.get();
+	raylib::Color background(RAYWHITE);
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+    raylib::Texture logo("raylib_logo.png");
+
+    SetTargetFPS(60);
+
+    while (!window.ShouldClose())
+    {
+        BeginDrawing();
+
+        background.ClearBackground();
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        // Object methods.
+        logo.Draw(
+            screenWidth / 2 - logo.GetWidth() / 2,
+            screenHeight / 2 - logo.GetHeight() / 2);
+
+        EndDrawing();
+    }
+
+    // UnloadTexture() and CloseWindow() are called automatically.
+
+    return 0;
 }
